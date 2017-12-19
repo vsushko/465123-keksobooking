0dc0a6c45d15902b4136b28c8e281e62690d3737
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-12-20 03:27:24 +0600

    add following filters treatment

--- js/filter.js
+++ js/filter.js
@@ -8,6 +8,12 @@
   // пины
   var mapPins = document.querySelector('.map__pins');
 
+  var housingTypeSelect = document.querySelector('#housing-type');
+  var housingPriceSelect = document.querySelector('#housing-price');
+  var housingRoomsSelect = document.querySelector('#housing-rooms');
+  var housingGuestsSelect = document.querySelector('#housing-guests');
+  var housingFeatures = document.querySelector('#housing-features');
+
   // пины для фильтрации
   var filteringPins;
 
@@ -20,30 +26,74 @@
     // объявления для фильтрации
     var advertisements = window.data.advertisements;
 
+    var selectedText;
+
     filteringPins.filter(function (mapPin) {
-
-      switch (event.target.id) {
-        case 'housing-type':
-          var selectedText = event.target.options[event.target.selectedIndex].text;
-          var currentPinValue = window.card.HOUSE_TYPES_MAP[mapPin.data.offer.type];
-
-          if (currentPinValue !== selectedText) {
-            mapPin.classList.add('hidden');
-          } else if (mapPin.classList.contains('hidden')) {
-            mapPin.classList.remove('hidden');
-          }
-          break;
-        case 'housing-price':
-          break;
-        case 'housing-rooms':
-          break;
-        case 'housing-guests':
-          break;
-        case 'housing-features':
-          break;
-        default:
-          break;
+      if (mapPin.classList.contains('hidden')) {
+        mapPin.classList.remove('hidden');
       }
+
+      var filtered = false;
+
+      // селектор типа жилья
+      var housingTypeSelector = housingTypeSelect.options[housingTypeSelect.selectedIndex];
+      var currentHousingType = window.card.HOUSE_TYPES_MAP[mapPin.data.offer.type];
+      var isAnyHousingTypeSelected = housingTypeSelector.value === 'any';
+
+      if (currentHousingType !== housingTypeSelector.text && !(isAnyHousingTypeSelected)) {
+        filtered = true;
+      }
+
+      // селектор цены
+      var housingPriceSelectorValue = housingPriceSelect.options[housingPriceSelect.selectedIndex].value;
+      var currentHousingPrice = mapPin.data.offer.price;
+
+      if (housingPriceSelectorValue === 'middle' && !(currentHousingPrice <= 50000 && currentHousingPrice >= 10000)) {
+        filtered = true;
+      } else if (housingPriceSelectorValue === 'low' && !(currentHousingPrice < 10000)) {
+        filtered = true;
+      } else if (housingPriceSelectorValue === 'high' && !(currentHousingPrice > 50000)) {
+        filtered = true;
+      } else if (!filtered && !(housingPriceSelectorValue === 'any')) {
+        filtered = false;
+        mapPin.classList.remove('hidden');
+      }
+
+      // селектор числа комнат
+      var housingRoomsSelectorValue = housingRoomsSelect.options[housingRoomsSelect.selectedIndex].value;
+      var currentHousingRooms = mapPin.data.offer.rooms;
+      var isAnyRoomsSelected = housingRoomsSelectorValue === 'any';
+
+      if (parseInt(housingRoomsSelectorValue, 10) !== currentHousingRooms && !(isAnyRoomsSelected)) {
+        filtered = true;
+      }
+
+      // селектор числа гостей
+      var housingGuestsSelectorValue = housingGuestsSelect.options[housingGuestsSelect.selectedIndex].value;
+      var currentHousingGuests = mapPin.data.offer.guests;
+      var isAnyGuestsSelected = housingGuestsSelectorValue === 'any';
+
+      if (parseInt(housingGuestsSelectorValue, 10) !== currentHousingGuests && !(isAnyGuestsSelected)) {
+        filtered = true;
+      }
+
+      // чекбоксы фич
+      var featuresCheckboxes = housingFeatures.querySelectorAll("input[type=checkbox]");
+
+      for (var i = 0; i < featuresCheckboxes.length; i++) {
+        var featureCheckbox = featuresCheckboxes[i];
+        if (featureCheckbox.checked && !(mapPin.data.offer.features.indexOf(featureCheckbox.value) > -1)) {
+          filtered = true;
+        }
+      }
+
+      if (filtered) {
+        mapPin.classList.add('hidden');
+      } else {
+        mapPin.classList.remove('hidden');
+      }
+
+      // скрыть карточку объявления если есть активная
     });
   });
 
