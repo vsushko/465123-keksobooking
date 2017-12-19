492fbfd9c3fab3bbff7b1b9ef0f4d2b7476297e2
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-12-20 05:31:32 +0600

    removed unused methods

--- js/util.js
+++ js/util.js
@@ -4,57 +4,6 @@
 (function () {
   window.util = {
     /**
-     * Перемешивает массив случайным образом
-     * @param {Array} arrayWithElements массив элементы которого нужно перемешать
-     * @return {Array} перемешанный массив
-     */
-    shuffleArray: function (arrayWithElements) {
-      var currentIndex = arrayWithElements.length;
-      var temporaryValue;
-      var randomIndex;
-
-      while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        temporaryValue = arrayWithElements[currentIndex];
-        arrayWithElements[currentIndex] = arrayWithElements[randomIndex];
-        arrayWithElements[randomIndex] = temporaryValue;
-      }
-      return arrayWithElements;
-    },
-    /**
-    * Возвращает случайное число в указанном интервале
-    * @param {Number} min минимальное число
-    * @param {Number} max максимальное числа
-    * @return {Number} случайное значение
-    */
-    getRandomIntInclusive: function getRandomIntInclusive(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
-    /**
-     * Возвращает массив строк случайного размера который не больше размера переданного из уникальных значений переданного
-     * @param {Array} arrayWithStrings массив строк
-     * @return {Array} массив уникальных строк случайной длины
-     */
-    getRandomizedArrayWithVariableLength: function (arrayWithStrings) {
-      var arrayWithRandomSize = [];
-      var randomArraySize = this.getRandomIntInclusive(1, arrayWithStrings.length - 1);
-
-      while (arrayWithRandomSize.length < randomArraySize) {
-        var randomNumber = Math.floor(Math.random() * arrayWithStrings.length - 1) + 1;
-        var arrayWithStringsValue = arrayWithStrings[randomNumber];
-
-        if (arrayWithRandomSize.indexOf(arrayWithStringsValue) > -1) {
-          continue;
-        }
-        arrayWithRandomSize.push(arrayWithStringsValue);
-      }
-      return arrayWithRandomSize;
-    },
-    /**
      * Удаляет у элементов контейнера указанные классы
      * @param {Array} elementWithClasses контейнер с объектами у которых нужно удалить класс
      * @param {String} classToRemove имя класса
