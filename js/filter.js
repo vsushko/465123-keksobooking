5072bc8a38a5fe6aa54c0516561c45a59337919c
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-12-21 21:56:12 +0600

    refactoring

--- js/filter.js
+++ js/filter.js
@@ -17,6 +17,8 @@
   var housingRoomsSelect = document.querySelector('#housing-rooms');
   var housingGuestsSelect = document.querySelector('#housing-guests');
   var housingFeatures = document.querySelector('#housing-features');
+  // чекбоксы
+  var featuresCheckboxes = housingFeatures.querySelectorAll('input[type=checkbox]');
 
   // добавим обработчик который будет отлавливать изменения на панели фильтров
   mapFilters.addEventListener('change', function () {
@@ -31,6 +33,78 @@
   });
 
   /**
+   * Возвращает результат необходимости фильтрации значения поля селектора
+   * @param {Object} currentSelectorValue выбранное значение селектора
+   * @param {Object} currentObjectValue текущее значение объекта
+   * @return {Object} результат сравнения
+   */
+  var checkSelectorValueForFiltering = function (currentSelectorValue, currentObjectValue) {
+    var filtered = false;
+
+    if (parseInt(currentSelectorValue, 10) !== currentObjectValue && !(currentSelectorValue === SELECTOR_ANY_VALUE)) {
+      filtered = true;
+    }
+
+    return filtered;
+  }
+
+  /**
+   * Возвращает результат необходимости фильтрации текста поля селектора
+   * @param {Object} currentSelectorValue выбранное значение селектора
+   * @param {Object} currentObjectValue текущее значение объекта
+   * @return {Object} результат сравнения
+   */
+  var checkSelectorTextForFiltering = function (currentSelector, currentObjectValue) {
+    var filtered = false;
+
+    if (currentSelector.text !== currentObjectValue && !(currentSelector.value === SELECTOR_ANY_VALUE)) {
+      filtered = true;
+    }
+
+    return filtered;
+  }
+
+  /**
+   * Возвращает результат необходимости фильтрации значения поля селектора цены
+   * @param {Object} currentSelectorValue1 выбранное значение селектора
+   * @param {Object} currentObjectValue текущее значение объекта
+   * @return {Object} результат сравнения
+   */
+  var checkPriceSelectorValuesForFiltering = function (currentSelectorValue1, currentObjectValue) {
+    debugger
+    var filtered = false;
+    if (!(currentSelectorValue1 === SELECTOR_ANY_VALUE)) {
+      if (currentSelectorValue1 === 'middle' && !(currentObjectValue <= 50000 && currentObjectValue >= 10000)) {
+        filtered = true;
+      } else if (currentSelectorValue1 === 'low' && !(currentObjectValue < 10000)) {
+        filtered = true;
+      } else if (currentSelectorValue1 === 'high' && !(currentObjectValue > 50000)) {
+        filtered = true;
+      }
+    }
+    return filtered;
+  }
+
+  /**
+   * Возвращает результат необходимости фильтрации по чекбоксам
+   * @param {Object} checkboxes
+   * @param {Object} currentObjectValue
+   * @return {Object} результат сравнения
+   */
+  var checkFeatureCheckBoxesForFiltering = function (checkboxes, currentObjectValue) {
+    var filtered = false;
+
+    for (var i = 0; i < featuresCheckboxes.length; i++) {
+      var featureCheckbox = featuresCheckboxes[i];
+
+      if (featureCheckbox.checked && !(currentObjectValue.indexOf(featureCheckbox.value) > -1)) {
+        filtered = true;
+      }
+    }
+    return filtered;
+  }
+
+  /**
    * Фильтрует пины
    * @param {Object} toFilteringPins пины для фильтрации
    */
@@ -41,57 +115,18 @@
     toFilteringPins.filter(function (mapPin) {
       var filtered = false;
 
-      // селектор типа жилья
-      var housingTypeSelector = housingTypeSelect.options[housingTypeSelect.selectedIndex];
-      var currentHousingType = window.card.houseTypeMap[mapPin.data.offer.type];
-      var isAnyHousingTypeSelected = housingTypeSelector.value === SELECTOR_ANY_VALUE;
-
-      if (currentHousingType !== housingTypeSelector.text && !(isAnyHousingTypeSelected)) {
-        filtered = true;
-      }
-
-      // селектор цены
-      var housingPriceSelectorValue = housingPriceSelect.options[housingPriceSelect.selectedIndex].value;
-      var currentHousingPrice = mapPin.data.offer.price;
-      var isAnyHousingPriceSelected = housingPriceSelectorValue === SELECTOR_ANY_VALUE;
-
-      if (!isAnyHousingPriceSelected) {
-        if (housingPriceSelectorValue === 'middle' && !(currentHousingPrice <= 50000 && currentHousingPrice >= 10000)) {
-          filtered = true;
-        } else if (housingPriceSelectorValue === 'low' && !(currentHousingPrice < 10000)) {
-          filtered = true;
-        } else if (housingPriceSelectorValue === 'high' && !(currentHousingPrice > 50000)) {
-          filtered = true;
-        }
-      }
-
       // селектор числа комнат
-      var housingRoomsSelectorValue = housingRoomsSelect.options[housingRoomsSelect.selectedIndex].value;
-      var currentHousingRooms = mapPin.data.offer.rooms;
-      var isAnyRoomsSelected = housingRoomsSelectorValue === SELECTOR_ANY_VALUE;
-
-      if (parseInt(housingRoomsSelectorValue, 10) !== currentHousingRooms && !(isAnyRoomsSelected)) {
-        filtered = true;
-      }
-
+      var isHouseTypePassed = checkSelectorTextForFiltering(housingTypeSelect.options[housingTypeSelect.selectedIndex], window.card.houseTypeMap[mapPin.data.offer.type]);
+      // селектор цены
+      var isHousePricePassed = checkPriceSelectorValuesForFiltering(housingPriceSelect.options[housingPriceSelect.selectedIndex].value, mapPin.data.offer.price);
+      // селектор числа комнат
+      var isHouseRoomsPassed = checkSelectorValueForFiltering(housingRoomsSelect.options[housingRoomsSelect.selectedIndex].value, mapPin.data.offer.rooms);
       // селектор числа гостей
-      var housingGuestsSelectorValue = housingGuestsSelect.options[housingGuestsSelect.selectedIndex].value;
-      var currentHousingGuests = mapPin.data.offer.guests;
-      var isAnyGuestsSelected = housingGuestsSelectorValue === SELECTOR_ANY_VALUE;
-
-      if (parseInt(housingGuestsSelectorValue, 10) !== currentHousingGuests && !(isAnyGuestsSelected)) {
-        filtered = true;
-      }
-
+      var isHouseGuestsPassed = checkSelectorValueForFiltering(housingGuestsSelect.options[housingGuestsSelect.selectedIndex].value, mapPin.data.offer.guests);
       // чекбоксы фич
-      var featuresCheckboxes = housingFeatures.querySelectorAll('input[type=checkbox]');
+      var isCheckBoxPassed = checkFeatureCheckBoxesForFiltering(featuresCheckboxes, mapPin.data.offer.features);
 
-      for (var i = 0; i < featuresCheckboxes.length; i++) {
-        var featureCheckbox = featuresCheckboxes[i];
-        if (featureCheckbox.checked && !(mapPin.data.offer.features.indexOf(featureCheckbox.value) > -1)) {
-          filtered = true;
-        }
-      }
+      filtered = isHouseTypePassed || isHousePricePassed || isHouseRoomsPassed || isHouseGuestsPassed || isCheckBoxPassed;
 
       if (filtered || visiblePinsCount >= MAX_PINS_AMOUNT_TO_SHOW - 1) {
         mapPin.classList.add(HIDDEN_CLASS_NAME);
